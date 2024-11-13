b6bf9e84b10b5e01731bab75bd7a08b9b452600d
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2024-11-13 12:35:13 +0100

    add navbar component with cscc and html

--- sakura/src/app/app.component.ts
+++ sakura/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { landigPageComponent} from './landigPage/landigPage.component'
+import { LandigPageComponent} from './landigPage/landigPage.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, landigPageComponent],
+  imports: [CommonModule, RouterOutlet, LandigPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- sakura/src/app/landigPage/landigPage.component.ts
+++ sakura/src/app/landigPage/landigPage.component.ts
@@ -1,18 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
     selector: 'app-landigPage',
     standalone: true,
-    imports: [CommonModule,],
-    template: `
-    <section>
-    <h1>Hello World!</h1>
-    <h2>Best Ramen in Town</h2>
-    </section>    
-    `,
+    imports: [CommonModule, NavbarComponent],
+    templateUrl : './landigPage.component.html',
     styleUrls: ['./landigPage.component.scss'],
 })
-export class landigPageComponent {
-
-}
+export class LandigPageComponent {}
