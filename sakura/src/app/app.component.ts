d263d10622c2835543f9a4761ed1ad4299a1c876
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2024-11-14 13:47:14 +0100

    leaned how to use router and now how set startside

--- sakura/src/app/app.component.ts
+++ sakura/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { MainContentComponent } from "./main-content/main-content.component";
 import { FooterComponent } from './shared/components/footer/footer.component';
-import { ImprintComponent } from './imprint/imprint.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MainContentComponent,FooterComponent,ImprintComponent],
+  imports: [CommonModule, RouterOutlet,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- sakura/src/app/app.routes.ts
+++ sakura/src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { MainContentComponent } from './main-content/main-content.component';
 import { ImprintComponent } from './imprint/imprint.component';
 
 export const routes: Routes = [
-    { path: 'mainside', component: MainContentComponent},
+    { path: '', component: MainContentComponent},
     { path: 'Impressum', component: ImprintComponent}
 ];
